1a446924185ff3f7c47435fa66445187cd0f6c7d
rakhil <rakhilpr96@gmail.com>
2020-09-07 00:40:26 +0530

    ocr

--- ocr/settings.py
+++ ocr/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,6 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
     ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
